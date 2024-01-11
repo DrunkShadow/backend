4894d12de97df7864ae053ab57aea42901209012
Taha Hamrouni <tahah680@gmail.com>
2024-01-11 14:40:54 +0100

    BACKEND WORKS

--- src/Controller/ProjectController.php
+++ src/Controller/ProjectController.php
@@ -3,46 +3,27 @@
 namespace App\Controller;
 
 use App\Entity\Project;
-use DateTime;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Validator\Constraints\Date;
-
+use Swagger\Annotations as SWG;
 class ProjectController extends AbstractController
 {
-    #[Route('/project', name: 'app_project')]
-    #public function createProject(EntityManagerInterface $entityManager): Response
+//     
 
-    #{
-        function show(EntityManagerInterface $entityManager): Response
-        {
-            $id =1;
-            $Project = $entityManager->getRepository(Project::class)->find($id);
-            if (!$Project) {
-                throw $this->createNotFoundException(
-                    'No Project found for id '.$id
-                );
-            }
-    
-            return new Response('Check out this great Project: '.$Project->getName());
+    public function Get(EntityManagerInterface $entityManager): Response
+    {
+        $projects = $entityManager->getRepository(Project::class)->findAll();
 
-        // $project = new Project();
+        if (!$projects) {
+            // Return a 404 response if no projects are found
+            return $this->json(['error' => 'No projects found'], 404);
+        }
 
-        // $project->setId(1);
-        // $project->setCategory('Economy');
-        // $project->setName('Projecta1');
-        // $project->setBudget(1000.020);
-        // $date = new \DateTime('now', new \DateTimeZone('UTC'));
-
-        // $project->setDate($date);
-
-        // $entityManager->persist($project);
-
-        // $entityManager->flush();
-
-        // return new Response('Saved new project with id '.$project->getId());
-
+        return $this->json($projects);
     }
 }
+
+
+        
